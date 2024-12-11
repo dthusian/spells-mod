73325d9857b31f8309b6fadb88f9389c06802c1b
Adrian Wu <eattofueveryday@gmail.com>
2024-12-11 00:08:09 -0500

    switch from snowballs to fireballs and back

--- src/main/java/dev/wateralt/mc/weapontroll/mixin/FireballEntityMixin.java
+++ src/main/java/dev/wateralt/mc/weapontroll/mixin/FireballEntityMixin.java
@@ -3,7 +3,6 @@ package dev.wateralt.mc.weapontroll.mixin;
 import dev.wateralt.mc.weapontroll.projectile.Projectile;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.FireballEntity;
-import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -12,11 +11,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SnowballEntity.class)
-public abstract class SnowballEntityMixin {
+@Mixin(FireballEntity.class)
+public abstract class FireballEntityMixin {
   @Inject(method = "onCollision", at = @At("HEAD"), cancellable = true)
   private void onCollision(HitResult hitResult, CallbackInfo ci) {
-    SnowballEntity that = (SnowballEntity) (Object) this;
+    FireballEntity that = (FireballEntity) (Object) this;
     if(that.getStack().getItem().equals(Items.NETHER_STAR)) {
       ci.cancel();
       LivingEntity hit = null;
--- src/main/java/dev/wateralt/mc/weapontroll/projectile/Projectile.java
+++ src/main/java/dev/wateralt/mc/weapontroll/projectile/Projectile.java
@@ -6,34 +6,36 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Projectile {
-  public static SnowballEntity create(ServerWorld world, LivingEntity caster, Vec3d pos, Vec3d dir, String spell) {
+  public static FireballEntity create(ServerWorld world, LivingEntity caster, Vec3d pos, Vec3d dir, String spell) {
     Vec3d vel = dir.normalize().multiply(0.1);
-    SnowballEntity ent = new SnowballEntity(EntityType.SNOWBALL, world);
+    FireballEntity ent = new FireballEntity(EntityType.FIREBALL, world);
     ent.setOwner(caster);
     ent.setPosition(pos.add(dir.normalize()));
     ent.setVelocity(vel);
-    ent.setNoGravity(true);
     ItemStack stack = new ItemStack(Items.NETHER_STAR, 1);
     NbtCompound nbt = new NbtCompound();
     nbt.putString("weapontroll_spell", spell);
     stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbt));
     ent.setItem(stack);
+    ent.setBoundingBox(new Box(pos.add(0.5), pos.subtract(0.5)));
     world.spawnEntity(ent);
     return ent;
   }
   
-  public static void execute(SnowballEntity ent, LivingEntity target) {
+  public static void execute(FireballEntity ent, LivingEntity target) {
     NbtComponent component = ent.getStack().get(DataComponentTypes.CUSTOM_DATA);
     if(component == null) return;
     AtomicReference<String> src = new AtomicReference<>();
